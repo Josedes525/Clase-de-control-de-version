b60d9c6904c458ee199547108288ead5c0424f8a
Josedes525 <inatec041@gmail.com>
2026-06-11 21:37:53 -0600

    Se añadió el archivo de código de python (promedio)

---
+++ Promedio.py
@@ -0,0 +1,7 @@
+print("bienvenido a mi promedio de notas")
+nota1=int(input("escribe tu primera nota: "))
+nota2=int(input("escribe tu segunda nota: "))
+nota3=int(input("escribe tu tercera nota: "))
+
+promedio=(nota1+nota2+nota3)/3
+print("tu promedio es de ",promedio)
